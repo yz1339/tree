807c3fb9905d41d53bad49ed5e86085a6f665206
yz1339 <yz1339@nyu.edu>
2016-10-04 01:46:03 +0000

    lol

--- pytree.py
+++ pytree.py
@@ -7,47 +7,47 @@ import os
 
 
 def tree(directory):
-	dlist = sorted(os.listdir(directory))
-	print(directory)
-	numOfDir = 0
-	numOfFile = 0
-	stack = []
-	for i in range(0, len(dlist)):
-		if (i != len(dlist) - 1):
-			prefix = '├── '
-		else:
-			prefix = '└── '
-		stack.append((dlist[i], prefix, 0))
-		discovered = []
-		while (len(stack) != 0):
-			d, prefix, indents = stack[-1]
-			stack = stack[:-1]
-			printD = d.split("/")[-1]
-			if (i == len(dlist) - 1 and indents > 0):
-				print('    ' + (indents - 1) * '│   ' + prefix + printD)
-			else:
-				print(indents * '│   ' + prefix + printD)
-			if d not in discovered:
-				discovered.append(d)
-				newDir = os.path.join(directory, d)
-				if os.path.isdir(newDir):
-					numOfDir += 1
-					sublist = sorted(os.listdir(newDir))
-					for j in range(len(sublist) - 1, -1, -1):
-						if (j != len(sublist) - 1):
-							prefix = '├── '
-						else:
-							prefix = '└── '
-						stack.append((d + "/" + sublist[j], prefix, indents + 1))
-				else:
-					numOfFile += 1
-	print()
-	print(str(numOfDir) + " directories, " + str(numOfFile) + " files")
+    dlist = sorted(os.listdir(directory))
+    print(directory)
+    numOfDir = 0
+    numOfFile = 0
+    stack = []
+    for i in range(0, len(dlist)):
+        if (i != len(dlist) - 1):
+            prefix = '├── '
+        else:
+            prefix = '└── '
+        stack.append((dlist[i], prefix, 0))
+        discovered = []
+        while (len(stack) != 0):
+            d, prefix, indents = stack[-1]
+            stack = stack[:-1]
+            printD = d.split("/")[-1]
+            if (i == len(dlist) - 1 and indents > 0):
+                print('    ' + (indents - 1) * '│   ' + prefix + printD)
+            else:
+                print(indents * '│   ' + prefix + printD)
+            if d not in discovered:
+                discovered.append(d)
+                newDir = os.path.join(directory, d)
+                if os.path.isdir(newDir):
+                    numOfDir += 1
+                    sublist = sorted(os.listdir(newDir))
+                    for j in range(len(sublist) - 1, -1, -1):
+                        if (j != len(sublist) - 1):
+                            prefix = '├── '
+                        else:
+                            prefix = '└── '
+                        stack.append((d + "/" + sublist[j], prefix, indents + 1))
+                else:
+                    numOfFile += 1
+    print()
+    print(str(numOfDir) + " directories, " + str(numOfFile) + " files")
 
 if __name__ == '__main__':
-	if (len(sys.argv) == 2):
-		tree(sys.argv[1])
-	elif (len(sys.argv) == 1):
-		tree('.')
-	else:
-		print("Please enter a directory!")
+    if (len(sys.argv) == 2):
+        tree(sys.argv[1])
+    elif (len(sys.argv) == 1):
+        tree('.')
+    else:
+        print("Please enter a directory!")
